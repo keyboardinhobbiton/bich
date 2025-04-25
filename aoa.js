dad5f9bb2266452cdaaf126d93c9d37852616a92
keyboardinhobbiton <dna67222@gmail.com>
2025-04-25 20:48:58 +0200

    Create aoa.js

---
+++ aoa.js
@@ -0,0 +1,20 @@
+{
+  "name": "shopping-assistant-ai",
+  "version": "1.0.0",
+  "description": "Shopping Assistant con Fake Store API e OpenAI",
+  "main": "app.js",
+  "scripts": {
+    "start": "node app.js",
+    "dev": "nodemon app.js"
+  },
+  "dependencies": {
+    "axios": "^1.6.2",
+    "body-parser": "^1.20.2",
+    "dotenv": "^16.3.1",
+    "express": "^4.18.2",
+    "openai": "^4.20.1"
+  },
+  "devDependencies": {
+    "nodemon": "^3.0.1"
+  }
+}
